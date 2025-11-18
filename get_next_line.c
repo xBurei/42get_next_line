79c63d3f262fc6965dda38d2ac62df06fc3edc28
v.fekete <vadimmarquez@gmail.com>
2025-11-18 13:02:50 +0100

    whole bunch of bullshit

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vfekete <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 17:57:12 by vfekete           #+#    #+#             */
-/*   Updated: 2025/11/18 12:13:33 by vfekete          ###   ########.fr       */
+/*   Updated: 2025/11/18 13:00:16 by vfekete          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+
+#ifndef BUFFER_SIZE
+ #define BUFFER_SIZE 20
+#endif
 
 t_line_buf *resize_line_buf(t_line_buf *line_buf)
 {
@@ -43,6 +50,9 @@ t_line_buf *init_line_buf(t_line_buf *line_buf)
 {
     size_t i;
 
+    line_buf = malloc(sizeof(t_line_buf));
+    if (!line_buf)
+        return (NULL);
     line_buf->content = malloc(BUFFER_SIZE);
     if (!line_buf->content)
         return (NULL);
@@ -58,6 +68,8 @@ int nl_found(t_rd_buf *rd_buf)
 {
     size_t i;
 
+    if (rd_buf->rd_out == 0)
+        return (0);
     i = 0;
     while (rd_buf->index + i < rd_buf->rd_out)
     {
@@ -74,19 +86,27 @@ int fill_read_buf(int fd, t_rd_buf *rd_buf)
         return (0);
 }
 
+void    clear_rd_buf(t_rd_buf *rd_buf)
+{
+    size_t  i;
+
+    i = 0;
+    while (i < BUFFER_SIZE)
+        rd_buf->content[i++] = 0;
+    rd_buf->index = 0;
+    rd_buf->rd_out = 0;
+}
+
 void append(t_line_buf *l_b, t_rd_buf *rd_b)
 {
-    size_t i;
+    int i;
 
-    if (nl_found(rd_b->content))
+    i = -1;
+    if (nl_found(rd_b))
     {
         // copy until newline
-        i = 0;
-        while (rd_b->content[rd_b->index + i] != '\n')
-        {
+        while (rd_b->content[rd_b->index + ++i] != '\n')
             l_b->content[l_b->index + i] = rd_b->content[rd_b->index + i];
-            i++;
-        }
         l_b->content[l_b->index + i] = rd_b->content[rd_b->index + i];
         i++;
         l_b->index += i;
@@ -95,21 +115,43 @@ void append(t_line_buf *l_b, t_rd_buf *rd_b)
     if (rd_b->rd_out != BUFFER_SIZE)
     {
         // copy until eof
+        while (rd_b->content[rd_b->index + ++i] < rd_b->rd_out)
+            l_b->content[l_b->index + i] = rd_b->content[rd_b->index + i];
+        l_b->index += i;
+        rd_b->index += i;
+        clear_rd_buf(rd_b);
     }
     else
     {
-        // resize_line_buf
+        resize_line_buf(l_b);
+        while (rd_b->content[rd_b->index + ++i] < BUFFER_SIZE)
+            l_b->content[l_b->index + i] = rd_b->content[rd_b->index + i];
+        clear_rd_buf(rd_b);
     }
 }
 
 char *get_next_line(int fd)
 {
     static t_rd_buf *rd_buf;
-    t_line_buf *line_buf;
+    t_line_buf      *line_buf;
 
     if (fd < 0)
         return (NULL);
-    line_buf = init_line_buf(line_buf);
+    init_line_buf(line_buf);
     if (!line_buf)
         return (NULL);
+    while (!nl_found(rd_buf) || rd_buf->rd_out != BUFFER_SIZE)
+    {
+        fill_read_buf(fd, rd_buf);
+        append(line_buf, rd_buf);
+    }
+    return (line_buf->content);
+}
+
+int main()
+{
+    int fd;
+
+    fd = open("testfile.txt", O_RDONLY, 0666);
+    printf("%s", get_next_line(fd));
 }
